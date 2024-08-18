59972793ebd16a8d745e5d13cdeef21044c348d9
Anish The Wizard <chunumunnu@gmail.com>
2024-08-18 05:15:00 -0400

    draft of simulation code

--- mcts.py
+++ mcts.py
@@ -11,18 +11,25 @@ class Node:
     self.children = {}
   
   def __repr__(self):
-    return f"{self.state}"
+    return f"\n{self.state}\n"
 
   def value(self) -> float:
     return self.value_sum / self.visits
   
   def expand(self, game: TicTacToe, model):
     action_board: np.ndarray = game.get_valid_moves(self.state)
-    priors, _ = model(self.state)
+    priors, value = model(self.state)
+    self.value_sum += value
     print("PRIOR PROB:", priors)
     for iy, ix in np.ndindex(action_board.shape):
       self.children[(iy, ix)] = Node(priors[iy * 3 + ix], 
                                    game.get_next_state(self.state, (iy, ix), -self.player), -self.player)
+  
+  def expanded(self):
+    return len(self.children) > 0
+  
+  def select_next_state(self) :
+    return self.children[list(self.children.keys())[0]]
   
 
 
@@ -37,6 +44,25 @@ class MCTS:
     root.expand(self.game, self.model)
 
     print("STATE:", root.state, '\nNODE CHILDREN:', root.children)
+    print("STARTING SIMULATION:")
+    
+    for simulation in range(num_simulations):
+      current_node = root
+      search_path = [current_node]
+      
+      while current_node.expanded():
+        current_node = current_node.select_next_state()
+        search_path.append(current_node)
+        
+      current_node.expand(self.game, self.model)
+      
+      self.backtrack(search_path, -1) # ??? what should the value be ???
+      
+  
+  def backtrack(self, search_path: list[Node], value):
+    for node in search_path:
+      node.value_sum += value
+      node.visits += 1
     
 
 if __name__ == "__main__":
